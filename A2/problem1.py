bf0ce8b0c44689553bf6630235c3142358772a4c
Tim Mooren <timmooren9@gmail.com>
2023-04-13 11:21:09 +0200

    init commit

---
+++ A2/problem1.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+"""
+NLP A2: N-Gram Language Models
+
+@author: Klinton Bicknell, Harry Eldridge, Nathan Schneider, Lucia Donatelli, Alexander Koller
+
+DO NOT SHARE/DISTRIBUTE SOLUTIONS WITHOUT THE INSTRUCTOR'S PERMISSION
+"""
+
+word_index_dict = {}
+
+# TODO: read brown_vocab_100.txt into word_index_dict
+with open("brown_vocab_100.txt", "r") as f:
+    for idx, line in enumerate(f.readlines()):
+        word = line.strip()
+        word_index_dict[word] = idx
+
+# TODO: write word_index_dict to word_to_index_100.txt
+with open("word_to_index_100.txt", "w") as f:
+    for word, idx in word_index_dict.items():
+        f.write(f"{word} {idx}")
+
+print(word_index_dict['all'])
+print(word_index_dict['resolution'])
+print(len(word_index_dict))
